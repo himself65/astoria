778e1a85a9796f02eaa2e9a5d71e99277bd49c95
himself65 <himself6565@gmail.com>
2018-12-07 02:01:56 +0800

    add koa-static to load frontend

--- config.example.js
+++ config.example.js
@@ -3,5 +3,6 @@ const port = process.env.PORT || 3000
 
 module.exports = {
   appName: "Himself65",
-  port: dev ? port : 80
+  port: dev ? port : 80,
+  distPath: '/root/himself65/dist'
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import Koa from 'koa'
+import server from 'koa-static'
+import path from 'path'
 
 // local config
 import config from '../config.example'
 
-const { port } = config
+const {
+  distPath = 'dist',
+  port
+} = config
 
 const app = new Koa()  // Singleton
 
 const astoria = {
   async run () {
-
+    app.use(server(path.resolve(distPath)))
     app.use(ctx => {
       ctx.body = 'This project have\'t finished.'
     })
