7cd52d74837eed07de02cd588879e4b77f0ab255
himself65 <himself6565@gmail.com>
2018-12-11 23:10:03 +0800

    ERR nodemon error action FIX config

--- src/utils/database.js
+++ src/utils/database.js
@@ -7,12 +7,14 @@ const db_url = `mongodb://localhost:${port}/${database}`
 
 export const connect_db = () => {
   console.log('Try to connect Mongoose.')
-  const db = mongoose.createConnection(db_url, { useNewUrlParser: true })
+  mongoose.connect(db_url, { useNewUrlParser: true } )
+  const db = mongoose.connection
   db.on('connected', () => {
     console.log(`Mongoose connection open to ${db_url}.`)
+    // todo: load Models
   })
   db.on('error', (err) => {
-    console.log('Mongoose connection error: ' + err)
+    console.error('Mongoose connection error: ' + err)
   })
   db.on('disconnected', () => {
     console.log('Mongoose connection disconnected')
