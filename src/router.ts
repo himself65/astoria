95bbd0370c4619a4a5605fc354ea79ceded7b4fa
himself65 <himself6565@gmail.com>
2018-12-16 01:42:40 +0800

    FIX if condition

--- src/router.ts
+++ src/router.ts
@@ -6,17 +6,21 @@ const router = new Router()
 const { apiUrl } = config
 
 for (const key in controllers) {
-  const controller = controllers[key]
-  const name = controller.name
-  const methods = controller.methods
-  for (const methodName in methods) {
-    if (methods[methodName] === null || methods[methodName] === undefined) {
-      console.error(`Got a nullable method on ${controller.name}.`)
-      continue
+  if (controllers.hasOwnProperty(key)) {
+    const controller = controllers[key]
+    const name = controller.name
+    const methods = controller.methods
+    for (const methodName in methods) {
+      if (methods.hasOwnProperty(methodName)) {
+        if (methods[methodName] === null || methods[methodName] === undefined) {
+          console.error(`Got a nullable method on ${controller.name}.`)
+          continue
+        }
+        const url = `${apiUrl}/${name}`
+        router[methodName](url, methods[methodName])
+        console.log(`register ${methodName} ${url}`)
+      }
     }
-    const url = `${apiUrl}/${name}`
-    router[methodName](url, methods[methodName])
-    console.log(`register ${methodName} ${url}`)
   }
 }
 
