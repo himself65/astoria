c6373d63cbdde7e0cda17e1cb37be55f2c2c2eb4
himself65 <himself6565@gmail.com>
2018-12-31 14:44:26 +0800

    UPDATE

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,32 @@
+import config from './config'
+
+const isProd = process.env.NODE_ENV === 'production'
+
+const apps = [{
+  name: 'astoria',
+  script: 'dist/index.js',
+
+  args: '',
+  instances: 1,
+  autorestart: true,
+
+  watch: false,
+  max_memory_restart: '1G',
+  env: {
+    NODE_ENV: 'development'
+  },
+  env_production: {
+    NODE_ENV: 'production'
+  }
+}]
+
+for (const k in config.repos) {
+  const repo = config.repos[k]
+  const configPath = `${repo}/ecosystem.config.js`
+  const apps = require(configPath).apps
+  apps.push(apps)
+}
+
 module.exports = {
-  apps: [{
-    name: 'astoria',
-    script: 'dist/src/index.js',
-
-    args: '',
-    instances: 1,
-    autorestart: true,
-
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
-    }
-  }]
+  apps
 }
