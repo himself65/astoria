40f929e4cd8ce23f3ecf8f1bdbd8db38cc33b75a
himself65 <himself6565@gmail.com>
2019-01-09 23:06:12 +0800

    [update] jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,22 @@
 module.exports = {
+  roots: [
+    '<rootDir>/src',
+    '<rootDir>/test/unit'
+  ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$'
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleDirectories: [
+    'node_modules'
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  mapCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,ts,tsx}',
+    '!**/*.d.ts'
+  ]
 }
