16db4d7d8bac7ec47f7d7c25876661ac138b0e9c
himself65 <himself6565@gmail.com>
2019-02-02 12:04:01 +0800

    abstract meta regex and its functions to module `md-meta`

--- src/utils/markdownParser.ts
+++ src/utils/markdownParser.ts
@@ -1,24 +1,18 @@
+import {
+  metaRegex, metaAllRegex,
+  metaLeftRegex, metaRightRegex,
+  parseMeta, removeMeta
+} from 'md-meta'
+
+export {
+  metaRegex, metaAllRegex, metaLeftRegex, metaRightRegex,
+  parseMeta, removeMeta
+}
+
 interface IPost {
   name: string | null
   meta: {}
   content: string
-}
-
-export const metaRegex = /(?<=^---\n)[\s\S]+?(?=---)/
-export const metaAllRegex = /^---[\s\S]+?(?<!\\)---/
-export const metaLeftRegex = /^.+?(?=(?<!\\):)/
-export const metaRightRegex = /(?<=(?<!\\):).+/
-
-export function removeMeta (markdown: string): string {
-  return metaAllRegex[Symbol.replace](markdown, '').trim()
-}
-
-export function parseMeta (data: string): {} {
-  const _ = {}
-  data.split('\n').forEach(line => {
-    _[metaLeftRegex.exec(line)[0].trim()] = metaRightRegex.exec(line)[0].trim()
-  })
-  return _
 }
 
 export function markdownParser (markdown: string): IPost {
