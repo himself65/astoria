0e43968e360b84a696dd97e4aeeb715a79d49bbe
himself65 <himself6565@gmail.com>
2019-02-07 20:56:07 +0800

    [update]

--- src/routers/article.ts
+++ src/routers/article.ts
@@ -1,25 +1,41 @@
 import * as KoaRouter from 'koa-router'
 import { debug } from '../'
 import { Article } from '../models/article'
+import { UserPermission } from '../utils/shared'
 
 export const router = new KoaRouter({ prefix: '/api' })
 
 router.post('/article', async (ctx) => {
-  const { content, title } = ctx.request.body
+  const { content, title, _id } = ctx.request.body
   const { username } = ctx.user
   debug(username, ctx.path, ctx.request.body)
   if (content && title && username) {
-    await Article.create({
-      author: username,
-      content,
-      title
-    }).then(res => {
-      if (res) {
-        ctx.response.status = 200
-      } else {
-        ctx.response.status = 400
-      }
-    })
+    if (_id) {
+      await Article.findById(_id).then(async (doc) => {
+        debug(doc)
+        if (!doc) {
+          ctx.response.status = 404
+        } else {
+          if (doc.author === ctx.user.username ||
+            ctx.user.level !== UserPermission.default) {
+            await Article.findByIdAndUpdate(_id, { content, title })
+          }
+          ctx.response.status = 200
+        }
+      })
+    } else {
+      await Article.create({
+        author: username,
+        content,
+        title
+      }).then(res => {
+        if (res) {
+          ctx.response.status = 200
+        } else {
+          ctx.response.status = 400
+        }
+      })
+    }
   } else {
     ctx.response.status = 400
   }
