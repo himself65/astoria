768ca6503a9c985e9b528320b8463216ff1182e5
himself65 <himself6565@gmail.com>
2019-02-08 15:12:08 +0800

    [update]

--- src/routers/accessRequired.ts
+++ src/routers/accessRequired.ts
@@ -3,8 +3,17 @@ import { UserPermission } from '../utils/shared'
 
 export const router = new KoaRouter()
 
+// access required
 router.all([/^\/backstage/, /^\/new/], async (ctx, next) => {
   if (ctx.user.level === UserPermission.default) {
+    ctx.response.redirect('/error')
+  }
+  await next()
+})
+
+// login required
+router.all(['/user'], async (ctx, next) => {
+  if (ctx.user.username === null) {
     ctx.response.redirect('/error')
   }
   await next()
